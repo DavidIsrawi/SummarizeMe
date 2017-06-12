fb422f65602e7e9e0550a059cc337f913c8e9786
David <davidisrawi@gmail.com>
2017-06-12 15:25:30 -0400

    main page + EB changes

--- server/application.py
+++ server/application.py
@@ -1,8 +1,10 @@
 from summarizeMe import summarize
 from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS, cross_origin
 import json
 
 application = Flask(__name__) # Define application
+CORS(application)
 
 @application.route('/', methods=['GET'])
 def index():
@@ -15,6 +17,7 @@ def sendStats():
     return jsonify({'statistics' : stats})
 
 @application.route('/summary', methods=['POST'])
+@cross_origin()
 def summary():
     result = summarize(request.json['text'])
     #result["stats"]["avg_contrast"] = "above"       # Temporary
