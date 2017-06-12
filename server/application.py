2bef59430df91f9f714379cdb4bbf89cfcd0c0b5
David <davidisrawi@gmail.com>
2017-06-12 12:06:06 -0400

    index added

---
+++ server/application.py
@@ -0,0 +1,25 @@
+from summarizeMe import summarize
+from flask import Flask, jsonify, request, render_template
+import json
+
+application = Flask(__name__) # Define application
+
+@application.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@application.route('/stats', methods=['GET'])
+def sendStats():
+    stats = json.load(open('db.json'))
+    #stats = { 'avg_current' : avg_current, 'num_summaries' : num_summaries }
+    return jsonify({'statistics' : stats})
+
+@application.route('/summary', methods=['POST'])
+def summary():
+    result = summarize(request.json['text'])
+    #result["stats"]["avg_contrast"] = "above"       # Temporary
+    #result["stats"]["avg_current"] = "40%"          # Temporary
+    return jsonify({'result' : result})
+
+if (__name__ == '__main__'):
+    application.run(debug=True, port=8080)
