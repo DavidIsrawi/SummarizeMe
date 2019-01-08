b0eef705606bed4a9acc7292a173eacc72c3050d
David <davidisrawi@gmail.com>
2019-01-07 18:16:39 -0800

    new API host

--- client/src/app/summary.service.ts
+++ client/src/app/summary.service.ts
@@ -16,7 +16,7 @@ export class SummaryService {
         // For rest api : http://127.0.0.1:8080/summary
         // For db.json : http://localhost:3000/db
         // For AWS Beanstalk: http://summarizeme.nrfkys4df7.us-east-1.elasticbeanstalk.com/stats
-        return this.http.post('http://summarizeme.nrfkys4df7.us-east-1.elasticbeanstalk.com/summary', this.textToSum)
+        return this.http.post('https://summarize-me.herokuapp.com/summary', this.textToSum)
             .map(res => res.json());
     }
 }
